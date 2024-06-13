ea7e3d5bc2ef11abbec7dc7234eef1c590b55aee
guptakaran11 <gupta.karan1162@gmail.com>
2024-06-13 21:49:30 +0530

    updation

--- lib/View/Screens/weather_page.dart
+++ lib/View/Screens/weather_page.dart
@@ -60,7 +60,7 @@ class _HomePageState extends State<WeatherPage> {
                       ),
                     ],
                   ),
-                  SizedBox(height: 50),
+                  SizedBox(height: 50,),
                 ],
               ),
             ),
