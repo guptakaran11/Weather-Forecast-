c07947b3dba284ee8c2132a4566d2c1536f93d6e
guptakaran11 <gupta.karan1162@gmail.com>
2024-06-21 18:52:59 +0530

    api integeration done

---
+++ lib/Controller/Services/api_keys.dart
@@ -0,0 +1,41 @@
+//* Dart Packages
+import "dart:convert";
+
+//* Packages
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter/material.dart';
+
+class WeatherService {
+  final String apiKey = "bca55f940425fd01d61ded1d545ce042";
+
+  Future<Map<String, dynamic>> getWeatherData(String cityName) async {
+    final response = await http.get(
+      Uri.parse(
+          'http://api.openweathermap.org/data/2.0/weather?q=$cityName&appid=$apiKey&units=metric'),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load Data');
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchWeatherData(String cityName) async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    double lat = position.latitude;
+    double lng = position.longitude;
+    final response = await http.get(
+      Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lng&appid=$apiKey&units=metric'),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load Data');
+    }
+  }
+}
