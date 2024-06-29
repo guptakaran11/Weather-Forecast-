98869c7b3edd312f788f35fb1edcd050ef09b90e
Karan Gupta <gupta.karan1162@gmail.com>
2024-06-29 12:58:20 +0530

    app completed

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,12 @@ class WeatherApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
         useMaterial3: true,
       ),
       home: const WeatherPage(),
     );
   }
 }
-
